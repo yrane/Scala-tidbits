270b860aa1b9d35ae4bd5b5f4bb40e5d6886e3a9
Yogesh Rane <yog2302@gmail.com>
2019-03-07 10:57:47 -0500

    Create CodeFrags.scala
    How to pass and accept variable-arguments(varargs).

---
+++ CodeFrags.scala
@@ -0,0 +1,10 @@
+
+class CodeFrags {
+  // how to accept variable-arguments(varargs) in Scala
+  def fn(vals: String*): Unit = {
+    vals.foreach(println)
+  }
+  val seq_a = Seq("A_KEY","B_KEY")
+  // To explode a list in parameters
+  fn(seq_a:_*)
+}
